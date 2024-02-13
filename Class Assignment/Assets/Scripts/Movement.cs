54805c7cd1d0d3e8179e5497a3aea804d7f39725
25dlee <136829451+25dlee@users.noreply.github.com>
2024-02-13 12:45:38 +0100

    edited

---
+++ Class Assignment/Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float runSpeed;
+    public Vector2 direction;
+    public bool jump;
+    public float jumpHeight;
+    public bool isGrounded;
+    public float groundDetectionRange;
+    public LayerMask groundMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(jump && isGrounded)
+        {
+            Jump();
+        }
+        FlipBehavior();
+    }
+
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+
+    void FlipBehavior()
+    {
+        if (direction.x != 0)
+        {
+            Vector3 currentScale = transform.localScale;
+            
+            transform.localScale = new Vector3(Mathf.Abs(currentScale.x) * Mathf.Sign(direction.x), currentScale.y, currentScale.z);  
+            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        GroundDetection();
+        rb.velocity = new Vector2(direction.x * runSpeed, rb.velocity.y);
+    }
+
+    void GroundDetection()
+    {
+        RaycastHit2D groundRay = Physics2D.Raycast(transform.position, Vector2.down, groundDetectionRange, groundMask);
+        isGrounded = groundRay.collider != null && (rb.velocity.y <= 0);
+    }
+}
